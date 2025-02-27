fb8dd0e75ef963e4146e6ec22a72ef9a2bfd4f6d
clarke <clarky1606chong@gmail.com>
2025-02-27 13:09:51 +0000

    implement UART commands start, stop, info. implement global variable timer_period_ms to prepare ability to modify timer period via UART command

--- src/slave_receive_uart.cpp
+++ src/slave_receive_uart.cpp
@@ -1,11 +1,10 @@
 void setup() {
     Serial.begin(115200);
-    Serial.println("Ready to receive data.");
   }
   
   void loop() {
     if (Serial.available() > 0) {
-      char receivedChar = Serial.read();
-      Serial.print(receivedChar);
+      char c = Serial.read();
+      Serial.print(c);
     }
   }
